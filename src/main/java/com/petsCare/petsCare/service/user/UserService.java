8bed818af09d0f4d1798532b5183f44b282a0bbc
jin <jintyjin15@gmail.com>
2023-12-19 17:32:22 +0900

    feat: Autowired UserRepository

--- src/main/java/com/petsCare/petsCare/service/user/UserService.java
+++ src/main/java/com/petsCare/petsCare/service/user/UserService.java
@@ -1,7 +1,12 @@
 package com.petsCare.petsCare.service.user;
 
+import com.petsCare.petsCare.repository.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
 }
