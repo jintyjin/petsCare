709407cc33b48ba5e5e64dacaf50821a49f4aedc
jin <jintyjin15@gmail.com>
2023-12-20 17:57:57 +0900

    feat: 로그인 컨트롤러 및 로그인 페이지 메서드 추가

---
+++ src/main/java/com/petsCare/petsCare/form/user/LoginForm.java
@@ -0,0 +1,22 @@
+package com.petsCare.petsCare.form.user;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class LoginForm {
+
+    @NotEmpty
+    @Size(min = 8, max = 16)
+    private String loginId;
+
+    @NotEmpty
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z[0-9]!@#$%^&*]{8,16}$")
+    private String password;
+
+    @NotEmpty
+    @Size(min = 8, max = 16)
+    private String nickName;
+}
