3a1d3c8995e7d90ee104d03685f31714ce74c278
jin <jintyjin15@gmail.com>
2023-12-26 16:10:21 +0900

    feat: 로그인 검증 기능 추가  - 회원 아이디, 비밀번호 validator 추가  - 로그인 후 저장될 세션명 추가  - 검증 오류 시 에러 메시지 추가

--- src/main/java/com/petsCare/petsCare/controller/LoginController.java
+++ src/main/java/com/petsCare/petsCare/controller/LoginController.java
@@ -1,18 +1,53 @@
 package com.petsCare.petsCare.controller;
 
+import com.petsCare.petsCare.entity.session.SessionConst;
+import com.petsCare.petsCare.entity.user.User;
 import com.petsCare.petsCare.form.user.LoginForm;
+import com.petsCare.petsCare.repository.user.UserRepository;
+import com.petsCare.petsCare.validation.loginForm.LoginFormLoginValidator;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
 
+    private final LoginFormLoginValidator loginFormLoginValidator;
+    private final UserRepository userRepository;
+
+    @InitBinder
+    public void init(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(loginFormLoginValidator);
+    }
+
     @GetMapping("/login")
     public String login(Model model) {
         model.addAttribute("loginForm", new LoginForm());
         return "/login/loginForm";
+    }
+
+    @PostMapping("/login")
+    public String login(@Validated LoginForm loginForm, BindingResult bindingResult, HttpServletRequest request,
+                        @RequestParam(defaultValue = "/") String redirectURL) {
+        if (bindingResult.hasErrors()) {
+            return "/login/loginForm";
+        }
+
+        User user = userRepository.findByLoginId(loginForm.getLoginId()).get();
+
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.LOGIN_USER, user);
+
+        return "redirect:" + redirectURL;
     }
 }
