4803bc975b7e2966eb9176ac34d39249f2184634
jin <jintyjin15@gmail.com>
2024-01-05 12:04:04 +0900

    feat: 추가 사항 - PetsCareApplication에 EnableJpaAuditing 어노테이션 누락 -> 추가 - InitDb를 이용해 기본 회원 데이터 1건 넣어놓음

--- src/main/java/com/petsCare/petsCare/PetsCareApplication.java
+++ src/main/java/com/petsCare/petsCare/PetsCareApplication.java
@@ -3,8 +3,10 @@ package com.petsCare.petsCare;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
+@EnableJpaAuditing
 public class PetsCareApplication {
 
 	public static void main(String[] args) {
