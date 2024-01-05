dac7ee8f7d6f17ce6cfbc6aee20f9eb254bd9eb2
jin <jintyjin15@gmail.com>
2024-01-05 11:42:15 +0900

    feat: UserService 구현 및 테스트 - ValidationGroups 클래스 추가 - ValidationSequence 인터페이스 추가 - UserJoinForm 수정 - UserService 회원 가입 기능 구현 - UserServiceTest 추가

--- src/main/java/com/petsCare/petsCare/service/user/UserService.java
+++ src/main/java/com/petsCare/petsCare/service/user/UserService.java
@@ -1,12 +1,43 @@
 package com.petsCare.petsCare.service.user;
 
+import com.petsCare.petsCare.entity.user.User;
+import com.petsCare.petsCare.exception.DuplicatedLoginIdException;
+import com.petsCare.petsCare.exception.DuplicatedNickNameException;
+import com.petsCare.petsCare.form.user.UserJoinForm;
 import com.petsCare.petsCare.repository.user.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
+@Slf4j
 public class UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void joinUser(UserJoinForm userJoinForm) {
+        User user = userJoinForm.createUser(passwordEncoder);
+
+        userRepository
+                .findByLoginId(user.getLoginId())
+                .ifPresent(byLoginId -> {
+                    throw new DuplicatedLoginIdException("existUser.userJoinForm.loginId");
+                });
+
+        userRepository
+                .findByNickName(user.getNickName())
+                .ifPresent(byNickName -> {
+                    throw new DuplicatedNickNameException("existUser.userJoinForm.nickName");
+                });
+
+        userRepository.save(user);
+    }
 }
