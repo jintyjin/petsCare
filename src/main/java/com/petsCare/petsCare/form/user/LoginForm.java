c29f1b5968f02da5bfee2f639f39004074db8788
jin <jintyjin15@gmail.com>
2024-01-07 08:30:24 +0900

    refactor: LoginForm 내부 정규식 @Pattern으로 통일 및 에러 메시지 추가

--- src/main/java/com/petsCare/petsCare/form/user/LoginForm.java
+++ src/main/java/com/petsCare/petsCare/form/user/LoginForm.java
@@ -1,18 +1,15 @@
 package com.petsCare.petsCare.form.user;
 
-import jakarta.validation.constraints.NotEmpty;
+import com.petsCare.petsCare.validation.ValidationGroups;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class LoginForm {
 
-    @NotEmpty
-    @Size(min = 5, max = 16)
+    @Pattern(regexp = "^[0-9a-zA-Z]{5,16}$", groups = ValidationGroups.PatternGroup.class)
     private String loginId;
 
-    @NotEmpty
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z[0-9]!@#$%^&*]{8,16}$")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z[0-9]!@#$%^&*]{8,16}$", groups = ValidationGroups.PatternGroup.class)
     private String password;
 }
