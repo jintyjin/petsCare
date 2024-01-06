907ba7f577ffc9b22e8c94e62a30688a870727f5
jin <jintyjin15@gmail.com>
2024-01-06 12:58:04 +0900

    chore: 회원 가입 시 오류 메시지 출력 하는 부분 수정
     - 아이디, 닉네임 중복 시 UserController 내 BindingResult에 오류 메시지 담는 로직 추가
     - 아이디 오류메시지 중 규칙 안내하는 부분 수정(영문, 숫자, 특수문자를 포함한 5 ~ 16자입니다. -> 5 ~ 16자의 영문, 숫자만 사용 가능합니다.)
     - 닉네임 오류메시지 중 규칙 안내하는 부분 수정(영문, 숫자, 특수문자를 포함한 5 ~ 16자입니다. -> 5 ~ 16자의 영문, 숫자, 한글만 사용 가능합니다.)

--- src/main/java/com/petsCare/petsCare/controller/UserController.java
+++ src/main/java/com/petsCare/petsCare/controller/UserController.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,9 +37,13 @@ public class UserController {
         try {
             userService.joinUser(userJoinForm);
         } catch (DuplicatedLoginIdException e) {
-            model.addAttribute("errorMessage", e.getMessage());
+            FieldError fieldError = new FieldError("userJoinForm", "loginId", e.getMessage());
+            bindingResult.addError(fieldError);
+            return "/user/userJoinForm";
         } catch (DuplicatedNickNameException e) {
-            model.addAttribute("errorMessage", e.getMessage());
+            FieldError fieldError = new FieldError("userJoinForm", "nickName", e.getMessage());
+            bindingResult.addError(fieldError);
+            return "/user/userJoinForm";
         }
 
         return "redirect:/login";
