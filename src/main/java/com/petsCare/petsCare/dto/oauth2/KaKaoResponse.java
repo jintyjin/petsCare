13617e321877e4f4d0f2f96035a503463878b777
jin <jintyjin15@gmail.com>
2024-02-06 17:12:00 +0900

    refactor: 로그인 정보에 name 추가
      - 혹여 발생할 수 있는 아이디 중복에 대비하여 유저 아이디 앞에 provider(검증 서버명) 추가

--- src/main/java/com/petsCare/petsCare/dto/oauth2/KaKaoResponse.java
+++ src/main/java/com/petsCare/petsCare/dto/oauth2/KaKaoResponse.java
@@ -21,6 +21,11 @@ public class KaKaoResponse implements OAuth2Response {
     }
 
     @Override
+    public String getName() {
+        return this.getProvider() + " " + this.getProviderId();
+    }
+
+    @Override
     public String getNickName() {
         return ((Map<String, Object>) attribute.get("properties")).get("nickname").toString();
     }
