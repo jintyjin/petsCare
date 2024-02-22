7b31ed70744ce44fbc3f57c9ff48c41e9419c84a
jinsejin <jintyjin15@gmail.com>
2024-02-22 21:48:33 +0900

    test: 펫 종 저장, 불러오기 테스트

---
+++ src/test/java/com/petsCare/petsCare/repository/pet/PetBreedRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.petsCare.petsCare.repository.pet;
+
+import com.petsCare.petsCare.entity.pet.PetBreed;
+import com.petsCare.petsCare.entity.pet.PetType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+@Transactional(readOnly = true)
+class PetBreedRepositoryTest {
+
+	@Autowired
+	PetBreedRepository petBreedRepository;
+
+	@Autowired
+	PetTypeRepository petTypeRepository;
+
+	@Test
+	@Transactional
+	@DisplayName("저장 및 불러오기 테스트")
+	void saveAndFind() {
+		//given
+		PetType dogType = new PetType("강아지");
+		PetType catType = new PetType("고양이");
+
+		petTypeRepository.save(dogType);
+		petTypeRepository.save(catType);
+
+		PetBreed dog1 = new PetBreed("닥스훈트", dogType);
+		PetBreed dog2 = new PetBreed("진돗개", dogType);
+		PetBreed tiger = new PetBreed("호랑이", catType);
+
+		//when
+		petBreedRepository.save(dog1);
+		petBreedRepository.save(dog2);
+		petBreedRepository.save(tiger);
+
+		List<PetBreed> dogs = petBreedRepository.findByPetType(dogType);
+		List<PetBreed> cats = petBreedRepository.findByPetType(catType);
+
+		//then
+		assertThat(dogs.size()).isEqualTo(2);
+		assertThat(cats.size()).isEqualTo(1);
+	}
+}
