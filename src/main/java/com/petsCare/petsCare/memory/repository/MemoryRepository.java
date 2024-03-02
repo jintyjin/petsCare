f14dbade1151f0b2e8c80c3d3565ae63d507f5b6
jinsejin <jintyjin15@gmail.com>
2024-03-02 11:06:50 +0900

    feat: 추억 가져오기 로직 추가

---
+++ src/main/java/com/petsCare/petsCare/memory/repository/MemoryRepository.java
@@ -0,0 +1,9 @@
+package com.petsCare.petsCare.memory.repository;
+
+import com.petsCare.petsCare.memory.dto.form.MemorySimpleForm;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface MemoryRepository {
+	Page<MemorySimpleForm> findSimpleFormByPet(Long petId, Pageable pageable);
+}
