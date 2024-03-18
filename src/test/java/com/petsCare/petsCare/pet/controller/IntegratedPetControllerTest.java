145e2f823983e544491bd54447bc9d6b5227458c
jinsejin <jintyjin15@gmail.com>
2024-03-18 18:20:20 +0900

    test: 펫 통합 테스트 수정
      - 인증 객체 수정 전 정상 -> 오류
      - oauth2Login().oauth2User()가 작동하지 않음
      - 추후 수정 예정

--- src/test/java/com/petsCare/petsCare/pet/controller/IntegratedPetControllerTest.java
+++ src/test/java/com/petsCare/petsCare/pet/controller/IntegratedPetControllerTest.java
@@ -2,6 +2,7 @@ package com.petsCare.petsCare.pet.controller;
 
 import com.petsCare.petsCare.oAuth2.dto.CustomOAuth2User;
 import com.petsCare.petsCare.user.dto.UserDto;
+import com.petsCare.petsCare.user.dto.form.UserJoinForm;
 import com.petsCare.petsCare.user.entity.User;
 import com.petsCare.petsCare.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -11,7 +12,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,11 +20,12 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 
 import static com.nimbusds.common.contenttype.ContentType.IMAGE_JPEG;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 @Transactional(readOnly = true)
 class IntegratedPetControllerTest {
 
@@ -39,7 +40,18 @@ class IntegratedPetControllerTest {
 	@DisplayName("반려 동물 등록 성공")
 	void adoptSuccess() throws Exception {
 		//given
-		User user = userRepository.findById(1L).get();
+		UserJoinForm userJoinForm = new UserJoinForm("testUser", "testUser!23", "진세진");
+
+		mockMvc.perform(
+				multipart("/join")
+						.param("loginId", userJoinForm.getLoginId())
+						.param("password", userJoinForm.getPassword())
+						.param("username", userJoinForm.getUsername())
+						.contentType(MediaType.MULTIPART_FORM_DATA)
+		);
+
+		User user = userRepository.findByLoginId(userJoinForm.getLoginId()).get();
+		UserDto userDto = new UserDto(user);
 		String url = "/pets/adopt";
 		String petName = "이복댕";
 		MockMultipartFile thumbnail = new MockMultipartFile("썸네일.png", "이복댕.png", IMAGE_JPEG.getType(), "이복댕.png".getBytes(StandardCharsets.UTF_8));
@@ -51,7 +63,7 @@ class IntegratedPetControllerTest {
 		//when
 		ResultActions resultActions = mockMvc.perform(multipart(url)
 						.file(thumbnail)
-						.with(SecurityMockMvcRequestPostProcessors.oauth2Login().oauth2User(new CustomOAuth2User(new UserDto(user))))
+				.with(oauth2Login().oauth2User(new CustomOAuth2User(userDto)))
 				.contentType(MediaType.MULTIPART_FORM_DATA)
 				.param("petName", petName)
 				.param("breed", breed)
@@ -69,12 +81,23 @@ class IntegratedPetControllerTest {
 	@DisplayName("반려 동물 불러오기 성공")
 	void petsSuccess() throws Exception {
 		//given
+		UserJoinForm userJoinForm = new UserJoinForm("testUser", "testUser!23", "진세진");
+
+		mockMvc.perform(
+				multipart("/join")
+						.param("loginId", userJoinForm.getLoginId())
+						.param("password", userJoinForm.getPassword())
+						.param("username", userJoinForm.getUsername())
+						.contentType(MediaType.MULTIPART_FORM_DATA)
+		);
+
 		String url = "/pets";
-		User user = userRepository.findById(1L).get();
+		User user = userRepository.findByLoginId(userJoinForm.getLoginId()).get();
+		UserDto userDto = new UserDto(user);
 
 		//when
 		ResultActions resultActions = mockMvc.perform(get(url)
-				.with(SecurityMockMvcRequestPostProcessors.oauth2Login().oauth2User(new CustomOAuth2User(new UserDto(user))))
+						.with(oauth2Login().oauth2User(new CustomOAuth2User(userDto)))
 		);
 
 		//then
