e210816844406ed57c6feaf5c4ed3c24ec5a51b6
jinsejin <jintyjin15@gmail.com>
2024-03-25 17:54:22 +0900

    refactor: AuthenticationPrincipal 대상 객체 수정

--- src/main/java/com/petsCare/petsCare/memory/controller/MemoryController.java
+++ src/main/java/com/petsCare/petsCare/memory/controller/MemoryController.java
@@ -1,8 +1,9 @@
 package com.petsCare.petsCare.memory.controller;
 
-import com.petsCare.petsCare.memory.dto.form.MemoryMakeForm;
-import com.petsCare.petsCare.memory.dto.form.MemorySimpleForm;
+import com.petsCare.petsCare.memory.dto.form.MemoryForm;
 import com.petsCare.petsCare.memory.service.MemoryService;
+import com.petsCare.petsCare.oAuth2.dto.CustomOAuth2User;
+import com.petsCare.petsCare.pet.service.PetService;
 import com.petsCare.petsCare.user.dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,27 +16,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/memory")
+@RequestMapping("/memories")
 @RequiredArgsConstructor
 public class MemoryController {
 
 	private final MemoryService memoryService;
+	private final PetService petService;
 
 	@GetMapping("/make")
-	public String make(Model model) {
-		model.addAttribute("memorySimpleForm", new MemorySimpleForm());
+	public String make(Model model, @AuthenticationPrincipal CustomOAuth2User oAuth2User) {
+		model.addAttribute("petIdAndNameForms", petService.showPets(oAuth2User.getUserDto().getId()));
+		model.addAttribute("memoryForm", new MemoryForm());
 
-		return "/memory/memoryMakeForm";
+		return "/memory/addMemoryForm";
 	}
 
 	@PostMapping("/make")
-	public String make(@Validated MemoryMakeForm memoryMakeForm, BindingResult bindingResult,
-					   @AuthenticationPrincipal UserDto userDto) {
+	public String make(@Validated MemoryForm memoryForm, BindingResult bindingResult,
+					   @AuthenticationPrincipal CustomOAuth2User oAuth2User, Model model) {
+		UserDto userDto = oAuth2User.getUserDto();
+
 		if (bindingResult.hasErrors()) {
-			return "/memory/memoryMakeForm";
+			model.addAttribute("petIdAndNameForms", petService.showPets(userDto.getId()));
+			return "/memory/addMemoryForm";
 		}
 
-		memoryService.make(memoryMakeForm, userDto);
+		memoryService.make(memoryForm, userDto);
 
 		return "redirect:/";
 	}
