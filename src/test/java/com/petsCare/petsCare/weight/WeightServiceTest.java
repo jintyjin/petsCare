2fa142fc6747157b88f72a80c75c768fda62f01d
jinsejin <jintyjin15@gmail.com>
2024-04-07 12:18:43 +0900

    test: 반려동물 체중 측정 비즈니스 로직 테스트 수정

--- src/test/java/com/petsCare/petsCare/weight/WeightServiceTest.java
+++ src/test/java/com/petsCare/petsCare/weight/WeightServiceTest.java
@@ -6,9 +6,7 @@ import com.petsCare.petsCare.pet.entity.PetType;
 import com.petsCare.petsCare.pet.repository.JpaPetRepository;
 import com.petsCare.petsCare.user.entity.User;
 import com.petsCare.petsCare.weight.dto.WeightForm;
-import com.petsCare.petsCare.weight.entity.Weight;
-import com.petsCare.petsCare.weight.repository.WeightRepository;
-import org.assertj.core.api.Assertions;
+import com.petsCare.petsCare.weight.repository.JpaWeightRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,13 +15,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.MessageSource;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
-import static java.util.Optional.of;
-import static java.util.Optional.ofNullable;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
@@ -34,7 +27,7 @@ class WeightServiceTest {
 	WeightService weightService;
 
 	@Mock
-	WeightRepository weightRepository;
+	JpaWeightRepository jpaWeightRepository;
 
 	@Mock
 	JpaPetRepository petRepository;
@@ -46,14 +39,14 @@ class WeightServiceTest {
 	@DisplayName("체중 측정 성공")
 	void measureSuccess() {
 		//given
-		lenient().when(petRepository.findById(anyLong()))
-				.thenReturn(of(new Pet("꼬맹이", null, new PetBreed("닥스훈트", new PetType("강아지")), 0,
-						LocalDate.now(), new User("provider", "진세진", "닉네임", "profileImage", "USER"))));
-
-		lenient().when(weightRepository.save(any(Weight.class)))
-				.thenReturn(any(Weight.class));
+		lenient().when(jpaWeightRepository.findPetForWeights(anyLong()))
+				.thenReturn(new Pet("꼬맹이", null, new PetBreed("닥스훈트", new PetType("강아지")), 0,
+						LocalDate.now(), new User("provider", "진세진", "닉네임", "profileImage", "USER")));
 
 		//when //then
-		assertThatCode(() -> weightService.measure(new WeightForm(1L, "이복댕", LocalDate.now(), new BigDecimal("8.5"), new ArrayList<>())));
+		assertThatCode(() -> weightService.measure(new WeightForm(
+				new Pet("꼬맹이", null, new PetBreed("닥스훈트", new PetType("강아지")), 0,
+						LocalDate.now(), new User("provider", "진세진", "닉네임", "profileImage", "USER"))
+		)));
 	}
 }
