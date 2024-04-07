927a47a473119805e46102e950f2b65201dd1a3c
jinsejin <jintyjin15@gmail.com>
2024-04-07 09:20:29 +0900

    test: 반려동물 체중 가져오기 로직 테스트

--- src/test/java/com/petsCare/petsCare/weight/repository/JpaWeightRepositoryTest.java
+++ src/test/java/com/petsCare/petsCare/weight/repository/JpaWeightRepositoryTest.java
@@ -9,6 +9,7 @@ import com.petsCare.petsCare.pet.repository.PetBreedRepository;
 import com.petsCare.petsCare.pet.repository.JpaPetRepository;
 import com.petsCare.petsCare.pet.repository.PetTypeRepository;
 import com.petsCare.petsCare.user.repository.UserRepository;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -20,7 +21,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
-class WeightRepositoryTest {
+class JpaWeightRepositoryTest {
 
     @Autowired
     UserRepository userRepository;
@@ -29,13 +30,16 @@ class WeightRepositoryTest {
 	JpaPetRepository jpaPetRepository;
 
     @Autowired
-    WeightRepository weightRepository;
+    JpaWeightRepository jpaWeightRepository;
 
     @Autowired
     PetTypeRepository petTypeRepository;
 
     @Autowired
     PetBreedRepository petBreedRepository;
+
+    @Autowired
+    EntityManager entityManager;
 
     @Test
     void 체중_등록() {
@@ -53,8 +57,8 @@ class WeightRepositoryTest {
 
         //when
         Weight weight = new Weight(new BigDecimal(9.28), LocalDate.now(), pet);
-        weightRepository.save(weight);
-        Weight findWeight = weightRepository.findById(weight.getId()).get();
+        jpaWeightRepository.save(weight);
+        Weight findWeight = jpaWeightRepository.findById(weight.getId()).get();
 
         //then
         assertThat(weight.getId()).isEqualTo(findWeight.getId());
@@ -81,15 +85,19 @@ class WeightRepositoryTest {
         Weight weight2 = new Weight(new BigDecimal(9.21), LocalDate.now(), pet);
         Weight weight3 = new Weight(new BigDecimal(9.35), LocalDate.now(), pet);
         Weight weight4 = new Weight(new BigDecimal(9.25), LocalDate.now(), pet);
-        weightRepository.save(weight1);
-        weightRepository.save(weight2);
-        weightRepository.save(weight3);
-        weightRepository.save(weight4);
+        jpaWeightRepository.save(weight1);
+        jpaWeightRepository.save(weight2);
+        jpaWeightRepository.save(weight3);
+        jpaWeightRepository.save(weight4);
+        Long petId = pet.getId();
+
+        entityManager.flush();
+        entityManager.clear();
 
         //when
-        List<Weight> findWeightsByPet = weightRepository.findByPet(pet);
+        Pet findPet = jpaWeightRepository.findPetForWeights(petId);
 
         //then
-        assertThat(findWeightsByPet.size()).isEqualTo(pet.getWeights().size());
+        assertThat(findPet.getWeights().size()).isEqualTo(4);
     }
 }
